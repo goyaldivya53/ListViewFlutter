7e5c390142fade25900b331ca245b12b7ec87046
goyaldivya53 <goyaldivya53@gmail.com>
2021-11-09 16:47:13 +0530

    ListView Project Flutter

---
+++ lib/main.dart
@@ -0,0 +1,131 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.grey ,
+          foregroundColor: Colors.white,
+        )
+      ),
+      home: RandomWords(),
+    );
+  }
+}
+
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
+  final _biggerFont = const TextStyle(fontSize: 16.0);
+  void _pushSaved(){
+    Navigator.of(context).push(
+      MaterialPageRoute(
+          builder: (context){
+            final tiles = _saved.map(
+                (pair){
+                  return ListTile(
+                    title: Text(
+                      pair.asPascalCase,
+                      style: _biggerFont,
+                    ),
+                  );
+                },
+            );
+            final divided = tiles.isNotEmpty
+                ?ListTile.divideTiles
+              (tiles: tiles,
+              context: context,
+            ).toList() :<Widget>[];
+
+            return Scaffold(
+              appBar: AppBar(
+                title: const Text('Saved Suggestions'),
+              ),
+              body: ListView(children: divided),
+            );
+          }
+
+      )
+    );
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+        actions: [
+          IconButton(
+              onPressed: _pushSaved,
+              icon: const Icon(Icons.list),
+            tooltip: "Saved Suggestions",
+          )
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+  Widget _buildSuggestions(){
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return const Divider(); /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair){
+    final alreadySaved = _saved.contains(pair);
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundColor: Colors.blue,
+      ),
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont ,
+      ),
+      trailing: Icon(
+       alreadySaved ? Icons.favorite : Icons.favorite_border,
+      color: alreadySaved ? Colors.red : null,
+      semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+    ),
+      onTap: (){
+        setState(() {
+          if(alreadySaved){
+            _saved.remove(pair);
+          }
+          else{
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+
+
+  }
+}
